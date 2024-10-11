6f8ca0e595138119bf98d7f4b9a4a4152483ccc1
Kyle Clark <kyleaclark@users.noreply.github.com>
2024-10-10 20:53:25 -0700

    parameterize brute knapsack tests

--- tests/unit/knapsack/test_brute_knapsack.py
+++ tests/unit/knapsack/test_brute_knapsack.py
@@ -1,37 +1,29 @@
+import pytest
+
 from src.knapsack.brute_knapsack import generate_knapsack
 
 
-def test_one():
-    items_dict = {"Lemons": 40, "Pennies": 20, "Milky Ways": 40, "Mounds": 50, "Apples": 25, "Crackers": 25}
-    weight_constraint = 100
+@pytest.mark.parametrize(
+    "items,weight_constraint,expected",
+    [
+        pytest.param(
+            {"Lemons": 40, "Pennies": 20, "Milky Ways": 40, "Mounds": 50, "Apples": 25, "Crackers": 25},
+            100,
+            [["Milky Ways", "Pennies", "Lemons"], ["Apples", "Mounds", "Crackers"]],
+        ),
+        pytest.param(
+            {"Snickers": 50, "Barbies": 65, "Butterfingers": 72}, 75, [["Snickers"], ["Butterfingers"], ["Barbies"]]
+        ),
+        pytest.param(
+            {"Starbursts": 54, "Barbies": 39, "Limes": 41, "Butterfingers": 11},
+            145,
+            [["Limes", "Starbursts", "Barbies", "Butterfingers"]],
+        ),
+    ],
+)
+def test_brute_knapsack(items, weight_constraint, expected):
+    result = [sorted(group, key=str.lower) for group in generate_knapsack(items, weight_constraint)]
+    expected = [sorted(group, key=str.lower) for group in expected]
 
-    knapsack = [sorted(group, key=str.lower) for group in generate_knapsack(items_dict, weight_constraint)]
-
-    expected_knapsack = [["Milky Ways", "Pennies", "Lemons"], ["Apples", "Mounds", "Crackers"]]
-    expected_knapsack = [sorted(group, key=str.lower) for group in expected_knapsack]
-
-    assert len(knapsack) == len(expected_knapsack) and sorted(knapsack) == sorted(expected_knapsack)
-
-
-def test_two():
-    items_dict = {"Snickers": 50, "Barbies": 65, "Butterfingers": 72}
-    weight_constraint = 75
-
-    knapsack = generate_knapsack(items_dict, weight_constraint)
-
-    expected_knapsack = [["Snickers"], ["Butterfingers"], ["Barbies"]]
-    expected_knapsack = [sorted(group, key=str.lower) for group in expected_knapsack]
-
-    assert len(knapsack) == len(expected_knapsack) and sorted(knapsack) == sorted(expected_knapsack)
-
-
-def test_three():
-    items_dict = {"Starbursts": 54, "Barbies": 39, "Limes": 41, "Butterfingers": 11}
-    weight_constraint = 145
-
-    knapsack = [sorted(group, key=str.lower) for group in generate_knapsack(items_dict, weight_constraint)]
-
-    expected_knapsack = [["Limes", "Starbursts", "Barbies", "Butterfingers"]]
-    expected_knapsack = [sorted(group, key=str.lower) for group in expected_knapsack]
-
-    assert len(knapsack) == len(expected_knapsack) and sorted(knapsack) == sorted(expected_knapsack)
+    assert len(result) == len(expected)
+    assert sorted(result) == sorted(expected)
